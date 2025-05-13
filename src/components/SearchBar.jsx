2a3a274f4d0a6e309135c5c9580a8e0fdc1715fa
Rolucatelli <rolucatelli@gmail.com>
2025-05-13 16:27:04 -0300

    Added the search icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { SearchContext } from "../store/search-context";
 import { Link, useNavigate } from "react-router-dom";
+import searchIcon from "../assets/magnifying-glass-solid.svg";
 
 export default function SearchBar(props) {
   const { querry, setQuerry } = useContext(SearchContext);
@@ -13,25 +14,27 @@ export default function SearchBar(props) {
 
   return (
     <div {...props}>
-      <input
-        className='p-2 h-12 w-sm border-blue-200 border-2 m-2'
-        type='text'
-        value={querry}
-        placeholder='Search...'
-        onChange={(event) => setQuerry(event.target.value)}
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            handleClick();
-          }
-        }}
-      />
+      <div className='flex items-center mt-6'>
+        <input
+          className='p-2 h-12 w-sm border-blue-200 border-2 mr-2'
+          type='text'
+          value={querry}
+          placeholder='Search...'
+          onChange={(event) => setQuerry(event.target.value)}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              handleClick();
+            }
+          }}
+        />
 
-      <button
-        onClick={handleClick}
-        className='rounded-md mt-5 p-3 text-gray-50 bg-green-500 cursor-pointer'
-      >
-        Search!
-      </button>
+        <button
+          onClick={handleClick}
+          className='rounded-md p-3 text-gray-50 bg-green-500 cursor-pointer flex items-center justify-center'
+        >
+          <img src={searchIcon} className='w-6 h-6' />
+        </button>
+      </div>
     </div>
   );
 }
